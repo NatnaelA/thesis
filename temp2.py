69464077a5cb28bf3ef4cb79354e265c28362744
NatnaelA <belayn@my.easternct.edu>
2019-02-03 17:49:58 -0500

    Update temp2.py

--- temp2.py
+++ temp2.py
@@ -13,47 +13,71 @@ co=community.k_clique_communities
 x=0
 y=0
 i=0
+k=0
 larg_pair=[]
+temp=[]
+
 t=3
 file=os.listdir('.')
+
+pairs=[]
 while x < len(file):
     
     line= file[x]
     
     name=os.listdir(line)
     #  os.path.isdir(line+'/inboxadfad'
-    if (os.path.isdir(line+'/inbox')):
+    if (os.path.isdir(line+'/_sent_mail')):
         
-        inbox=os.listdir(line + '/inbox')
-        while i<len(inbox):
+        sent=os.listdir(line + '/_sent_mail')
+        while i<len(sent):
             pairs=[]
-            if(os.path.isfile(line+'/inbox'+"/"+inbox[i])):
-                with open(os.path.join(line+'/inbox',inbox[i]),'r') as f:
+            if(os.path.isfile(line+'/_sent_mail'+"/"+sent[i])):
+                with open(os.path.join(line+'/_sent_mail',sent[i]),'r') as f:
             
                     f_contents=f.readlines()
                    # if(f_contents[3][:2]=='To'):
-                    p=0
-                    pairs.append(f_contents[2])
+                    
+                    
                     r=3
-            while(not("Subject:" in f_contents[r])):
+                    while(not("Subject:" in f_contents[r])):
+                       
+                       
+                       if("," in f_contents[r]):
+                           
+                            temp=f_contents[r].split(', ')
+                            k=0
+                            while(k<len(temp)):
+                               #pairs=[]
+                               pairs.append(f_contents[2])
+                               pairs.append(temp[k])
+                               larg_pair.append(pairs)
+                               pairs=[]
+                               k=k+1
+                               
+                       else:
+                           
+                            pairs.append(f_contents[2])
+                            pairs.append(f_contents[r])
+                            larg_pair.append(pairs)
+                        
+                       
+                       
                     
-                   
-                    pairs.append(f_contents[r])
-                    
-                    
-                    larg_pair.append(pairs)
-                    subject=line
-                    i=i+1
-                    p=p+1
-                    r=r+1
+                       
+                       
+                       
+                       r=r+1
+                      
             
                    # else:
                         
                     #    i=i+1
-            else:
-                i=i+1
-        
+              
+            r=0
+            i=i+1
+        i=0
         x=x+1
     else:
-                   y=y+1
-                   x=x+1
+                   
+        x=x+1
