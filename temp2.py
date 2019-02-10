78750886caa9948721f6951c6c6e36c0c2dd0285
NatnaelA <belayn@my.easternct.edu>
2019-02-10 15:42:35 -0500

    Add files via upload

--- temp2.py
+++ temp2.py
@@ -7,7 +7,9 @@ Created on Mon Sep 24 16:40:52 2018
 import os
 import networkx as nx
 from networkx.algorithms import community 
-os.chdir('C:/Users/Natna/Downloads/enron_mail_20150507.tar/maildir/')
+import argparse
+import sys
+#os.chdir('C:/Users/Natna/Downloads/enron_mail_20150507.tar/maildir/')
 G=nx.path_graph(10)
 co=community.k_clique_communities
 x=0
@@ -18,49 +20,84 @@ larg_pair=[]
 temp=[]
 
 t=3
-file=os.listdir('.')
+
 
 pairs=[]
+ap = argparse.ArgumentParser(description='directory to the email folder')
+ap.add_argument('input directory',help='Directroy of input files', default='C:/Users/Natna/Downloads/enron_mail_20150507.tar/maildir/')
+
+args = vars(ap.parse_args())
+file=os.listdir(args['input directory'])
+#file=directory
 while x < len(file):
     
     line= file[x]
     
-    name=os.listdir(line)
+    
     #  os.path.isdir(line+'/inboxadfad'
-    if (os.path.isdir(line+'/_sent_mail')):
+    if (os.path.isdir(args['input directory']+'/'+line+'/_sent_mail')):
         
-        sent=os.listdir(line + '/_sent_mail')
+        sent=os.listdir(args['input directory']+'/'+line + '/_sent_mail')
         while i<len(sent):
             pairs=[]
-            if(os.path.isfile(line+'/_sent_mail'+"/"+sent[i])):
-                with open(os.path.join(line+'/_sent_mail',sent[i]),'r') as f:
+            if(os.path.isfile(args['input directory']+'/'+line+'/_sent_mail'+"/"+sent[i])):
+                with open(os.path.join(args['input directory']+'/'+line+'/_sent_mail',sent[i]),'r') as f:
             
                     f_contents=f.readlines()
                    
+        
+                        
+                        
+                       
+                        
+                   # if(f_contents[3][:2]=='To'):
                     
                     
                     r=3
                     while(not("Subject:" in f_contents[r])):
                        
-                       
+                      
                        if("," in f_contents[r]):
-                           
-                            temp=f_contents[r].split(', ')
+                            f_contents[r].strip('\t')
+                            
+                            temp=f_contents[r].split(',')
+                            
                             k=0
                             while(k<len(temp)):
+                                if(temp[k] != ''):
                                #pairs=[]
-                               pairs.append(f_contents[2])
-                               pairs.append(temp[k])
-                               larg_pair.append(pairs)
-                               pairs=[]
-                               k=k+1
+                                   f_contents[2]=f_contents[2].strip()
+                                   f_contents[2]=f_contents[2].strip('From: ')
+                                   pairs.append(f_contents[2])
+                                   temp[k]=temp[k].strip()
+                                   temp[k]=temp[k].strip('To: ')
+                                   pairs.append(temp[k])
+                                   pairs.append(sent[i])
+                                   larg_pair.append(pairs)
+                                   
+                                   pairs=[]
+                                   k=k+1
+                                else:
+                                    print(temp[k])
+                                    k=k+1
+                                     
                                
                        else:
-                           
-                            pairs.append(f_contents[2])
-                            pairs.append(f_contents[r])
-                            larg_pair.append(pairs)
-                        
+                               if(f_contents[r] != ''):
+                                    f_contents[2]=f_contents[2].strip()
+                                    f_contents[2]=f_contents[2].strip('From: ')
+                                    pairs.append(f_contents[2])
+                                    
+                                    f_contents[r]=f_contents[r].strip()
+                                    f_contents[r]=f_contents[r].strip('To: ')
+                                    pairs.append(f_contents[r])
+                                    pairs.append(sent[i])
+                                    larg_pair.append(pairs)
+                               else:
+                                    r=r+1
+                                
+                                
+                                
                        
                        
                     
@@ -74,7 +111,7 @@ while x < len(file):
                         
                     #    i=i+1
               
-            r=0
+           
             i=i+1
         i=0
         x=x+1
