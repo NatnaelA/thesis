4dff40b475b48ae81c6b36d00b44c4072ee70e10
NatnaelA <belayn@my.easternct.edu>
2019-03-23 15:28:12 -0400

    Update temp2.py

--- temp2.py
+++ temp2.py
@@ -17,8 +17,8 @@ ap = argparse.ArgumentParser(description='directory to the email folder')
 ap.add_argument('--input', help='Directroy of input files', default=r'C:\Users\belayn\OneDrive - Eastern Connecticut State University\enron_mail_20150507\maildir', required = False)
 #ap.add_argument('--output', help='Directroy of output files', default='C:/Users/Natna/Documents', required = False)
 ap.add_argument('--output', help='Directroy of output files', default=r'C:\Users\belayn\OneDrive - Eastern Connecticut State University\enron_mail_20150507', required = False)
-ap.add_argument('--startdate', help='start date of range', default='1 1 1997', required = False)
-ap.add_argument('--enddate', help='end date of range', default='31 1 2001', required = False)
+ap.add_argument('--startdate', help='start date of range', default='1 1 1998', required = False)
+ap.add_argument('--enddate', help='end date of range', default='31 1 2002', required = False)
 args = vars(ap.parse_args())
 split_start_date=args['startdate'].split()
 split_end_date=args['enddate'].split()
@@ -51,7 +51,7 @@ sendDict = defaultdict(set)
 numFiles = 0
 numProcessed = 0
 all_sent=[]
-all_sent=['\\sent','\\_sent_mail','\\sent_items']
+all_sent=['\\sent','\\_sent_mail','\\sent_items','\\_sent']
 for line in file:
     numFiles += 1 
     print("checking folder #", numFiles, ': ', line)
@@ -69,7 +69,7 @@ for line in file:
                
                 if(os.path.isfile(sent_mail +"\\"+email)):
                    
-                   parse_Email.parseEmail(sent_mail +"\\"+email,['To','From','subject','body',{'start_date':start_date,'end_date':end_date}])
+                   parse_Email.parseEmail(sent_mail +"\\"+email,['To','From','Subject','Body',{'start_date':start_date,'end_date':end_date}])
                    
         
                              
@@ -88,8 +88,24 @@ with open(args['output']+'/'+'edgelist.csv','w+') as list:
    for item in parse_Email.larg_pair:
        edge_list=', '.join(item)
        list.write(edge_list+'\n')
+       
+print("outputting subjects to:", args['output'] + '/subjects.csv')
+with open(args['output']+'/'+'subjects.txt','w+') as list:
+   for item in parse_Email.subject:
+       if item is not '\n':
+           list.write(item+'\n')
+       
+print("outputting body to:", args['output'] + '/body.csv')
+with open(args['output']+'/'+'body.txt','w+') as list:
+   for item in parse_Email.contents:
+       
+       list.write(item+'\n'+'\n')
+with open(args['output']+'/'+'dates.csv','w+') as list:
+   for item in parse_Email.date:
+       
+       list.write(str(item.year)+'\n')
 #print('This is the subject'+parse_Email.subject[-1])
-print('This is the body'+parse_Email.body)
+
 for senders in sendDict :
     print(senders, ":" , sendDict[senders])
 
