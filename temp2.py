ba1313ce22013cb830cc6f67a91f62e54de96b41
NatnaelA <belayn@my.easternct.edu>
2019-03-22 22:18:54 -0400

    Update temp2.py

--- temp2.py
+++ temp2.py
@@ -4,22 +4,14 @@ Created on Mon Sep 24 16:40:52 2018
 @author: Natna
 """
 import os
-
+import parse_Email
 import argparse
 import sys
 from datetime import datetime 
 #os.chdir('C:/Users/Natna/Downloads/enron_mail_20150507.tar/maildir/')
 
-x=0
-y=0
-i=0
-k=0
-larg_pair=[]
-temp=[]
-month={1:'Jan',2:'Feb',3:'Mar',4:'Apr',5:'May',6:'Jun',7:'Jul',8:'Aug',9:'Sep',10:'Oct',11:'Nov',12:'Dec'}
-t=3
 
-pairs=[]
+
 ap = argparse.ArgumentParser(description='directory to the email folder')
 ap.add_argument('--input', help='Directroy of input files', default='C:/Users/Natna/Downloads/enron_mail_20150507.tar/maildir/', required = False)
 ap.add_argument('--output', help='Directroy of output files', default='C:/Users/Natna/Documents', required = False)
@@ -74,72 +66,27 @@ for line in file:
             for email in sent:
                 pairs=[]
                 if(os.path.isfile(sent_mail +"/"+email)):
-                    with open(os.path.join(sent_mail,email),'r') as f:
-                
-                        f_contents=f.readlines()
-                        
+                   parse_Email(sent_mail +"/"+email,['To','From','subject','body',{'start_date':start_date,'end_date':end_date}])
                             
-                        blocks=f_contents[1].split()
-                        for thing in blocks:
-                            for i in range(1,13):
+        
+                             
+                   
                             
-                                if thing == month[i]:
-                                    
-                                    blocks[3]=str(i)
-                                    temp_date=', '.join(blocks[2:5])
-                                    email_date=datetime.strptime(temp_date ,'%d, %m, %Y')
-                                    
-                                    
-                                    break 
-                            
-                    if(start_date<=email_date and email_date<=end_date ):
-                            
-                      
-                        
-                        r=3
-                        while(not("Subject:" in f_contents[r])):
-                           
-                          
-                           
-                                f_contents[r].strip('\t')
-                                
-                                
-                                temp=f_contents[r].split(',')
-                                
-                                k=0
-                                for employee in temp:
-                                    employee = employee.strip()
-                                    if(employee != ''):
-                                   
-                                       f_contents[2]=f_contents[2].strip('\n')
-                                       f_contents[2]=f_contents[2].replace('From: ','')
-                                       pairs.append(f_contents[2])
-                                       sendDict[line].add(f_contents[2])
-    
-                                       employee=employee.strip()
-                                       employee=employee.replace('To: ','')
-                                       pairs.append(employee)
-                                       pairs.append(email)
-                                       larg_pair.append(pairs)
-                                       
-                                       pairs=[]
-                                       
-                           
-                           
-                                r=r+1
                       
             
 print()
 print("Number of folders searched: ", numFiles)
 print("Number of folders processed: ", numProcessed)
+print("Number of folders skipped: ", numFiles - numProcessed)
 print()
 
 print("outputting edge list to:", args['output'] + '/edgelist.csv')
 with open(args['output']+'/'+'edgelist.csv','w+') as list:
-   for item in larg_pair:
+   for item in parse_Email.large_pair:
        edge_list=', '.join(item)
        list.write(edge_list+'\n')
-
+print('This is the subject'+parse_Email.subject)
+print('This is the body'+parse_Email.body)
 for senders in sendDict :
     print(senders, ":" , sendDict[senders])
 
